0ba14ae4bf2dc39767cc1da56b4d13299df57b61
Snehil <lohia.snehil1999@gmail.com>
2021-12-27 20:27:10 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io("https://chat-app-server-delta.vercel.app");
+const socket = io("https://chat-server-socketio1.herokuapp.com/");
 
 function App() {
   const [username, setUsername] = useState();
   const [room, setRoom] = useState();
   const [showChat, setShowChat] = useState();
 
-  const joinRoom = () => {
+  const joinRoom = (e) => {
+    e.preventDefault();
     if (username && room) {
       socket.emit("join_room", room);
       setShowChat(true);
@@ -21,18 +22,22 @@ function App() {
     <div className="App">
       {!showChat ? (
         <div className="join-chat">
-          <h3>Join a chat</h3>
-          <input
-            type="text"
-            placeholder="Your name:"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Room ID"
-            onChange={({ target }) => setRoom(target.value)}
-          />
-          <button onClick={joinRoom}>Join room</button>
+          <h3>JOIN A CHAT</h3>
+          <form onSubmit={joinRoom}>
+            <input
+              type="text"
+              placeholder="Your name:"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Room ID"
+              onChange={({ target }) => setRoom(target.value)}
+            />
+            <button type="submit" className="join-chat-btn">
+              Join room
+            </button>
+          </form>
         </div>
       ) : (
         <Chat socket={socket} username={username} room={room} />
