0c84d975e491915ca3e6cab44d0978ca8ff99b26
Snehil <lohia.snehil1999@gmail.com>
2022-01-05 19:04:28 +0530

    .

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { useState } from "react";
 import Chat from "./components/Chat";
 import Canvas from "./components/Canvas";
 
-// const socket = io("https://chat-server-socketio1.herokuapp.com/");
-const socket = io("http://localhost:3001", {
-  forceNew: true,
-});
+const socket = io("https://chat-server-socketio1.herokuapp.com/");
+// const socket = io("http://localhost:3001", {
+//   forceNew: true,
+// });
 
 function App() {
   const [username, setUsername] = useState();
